25d9f9966a04d42a213e1122a139615cbcbaa286
vtucker16 <47865762+vtucker16@users.noreply.github.com>
2021-10-04 17:13:43 -0400

    Add files via upload

---
+++ Assets/Trigger3.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger3 : MonoBehaviour
+{
+   private BoxCollider2D bc;
+
+   private void Awake(){
+       bc = GetComponent<BoxCollider2D>();
+       bc.isTrigger = true; 
+   }
+
+   void OnTriggerEnter2D(Collider2D collision){
+       //Debug.Log("A collision has happened.", gameObject);
+       if(collision.CompareTag("Player")){
+            GameObject.Find("Main Camera").transform.position = new Vector3(50, -49, -10);
+       }
+
+   }
+}
